2e8628e23ce2c16baed704be2d87a3320a416004
Jul Lang <jullanggit@proton.me>
2025-01-29 10:53:25 +0100

    add circle

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{io::Write, mem::transmute};
+use std::{fs, io::Write, mem::transmute};
 
 fn main() {
-    println!("Hello, world!");
+    let circle = Image::circle(30);
+    circle.write();
 }
 
 struct Image {
@@ -18,12 +19,41 @@ impl Image {
         writeln!(&mut buf, "{} {}", self.width, self.height).unwrap();
         buf.write_all(unsafe { transmute::<&[Pixel], &[u8]>(&self.data) }) // For some reason not yet supported by TransmuteFrom
             .unwrap();
+
+        fs::write("out.ppm", buf).unwrap();
+    }
+    fn circle(radius: u16) -> Self {
+        let diameter = radius * 2;
+        let mut data = Vec::with_capacity((diameter * diameter * 3) as usize);
+
+        for x in 0..diameter {
+            for y in 0..diameter {
+                if x * x + y * y < radius * radius {
+                    data.push(Pixel::WHITE);
+                } else {
+                    data.push(Pixel::BLACK)
+                }
+            }
+        }
+
+        Self {
+            width: diameter,
+            height: diameter,
+            data,
+        }
+    }
+    fn flatten_indices(&self, x: usize, y: usize) -> usize {
+        x + y * self.width as usize
     }
 }
 
 #[repr(C)]
 struct Pixel {
-    r: u8,
-    g: u8,
-    b: u8,
+    inner: [u8; 3],
+}
+impl Pixel {
+    const WHITE: Self = Self {
+        inner: [u8::MAX; 3],
+    };
+    const BLACK: Self = Self { inner: [0; 3] };
 }
