9403ba8fbc7f687296d3a8cb8f371bca135455fa
Jul Lang <jullanggit@proton.me>
2025-04-19 22:21:54 +0200

    add incremental rendering benchmark

--- benches/raytracer.rs
+++ benches/raytracer.rs
@@ -25,10 +25,24 @@ triangles()";
     c.bench_function("rendering", |b| b.iter(|| scene.render()));
 }
 
+fn incremental_rendering(c: &mut Criterion) {
+    let string = "incremental(2)
+screen(-1.15 1.75 3, 2 0 0, 0 -2 0, 1000, 1000, 10, 10)
+camera(0.15 0.75 30)
+spheres()
+planes()
+obj((bunny))
+triangles()";
+
+    let scene = SCENE.get_or_init(|| config::parse(string));
+
+    c.bench_function("incremental_rendering", |b| b.iter(|| scene.render()));
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = rendering
+    targets = rendering, incremental_rendering
 }
 
 criterion_main!(benches);
