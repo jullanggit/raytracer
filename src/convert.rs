ea788fd2e98dce358c98e74f58587ca7dd82ef06
Jul Lang <jullanggit@proton.me>
2025-09-02 14:16:22 +0200

    Override convert for floats using as

--- src/convert.rs
+++ src/convert.rs
@@ -8,6 +8,11 @@ impl<T> TryConvert<T> for T {
         Some(self)
     }
 }
+impl<T, O> TryConvert<O> for T {
+    default fn try_convert(self) -> Option<O> {
+        <Self as TryConvertSpec<O>>::try_convert(self)
+    }
+}
 
 impl<T, O: From<T>> TryConvert<O> for T {
     default fn try_convert(self) -> Option<O> {
@@ -21,6 +26,41 @@ impl<T, O: TryFrom<T>> TryConvert<O> for T {
     }
 }
 
+pub trait TryConvertSpec<Output> {
+    fn try_convert(self) -> Option<Output>;
+}
+
+macro_rules! FloatAsConvert {
+    ($head:ident, $($tail:ident),+) => {
+        $(
+            FloatAsConvertInner!($head, $tail);
+            FloatAsConvertInner!($tail, $head);
+        )+
+    };
+}
+macro_rules! FloatAsConvertInner {
+    ($a:ident, $b:ident) => {
+        impl TryConvertSpec<$b> for $a {
+            fn try_convert(self) -> Option<$b> {
+                Some(self as $b)
+            }
+        }
+    };
+}
+FloatAsConvert!(f16, f32, f64, f128);
+
+impl<T, O> TryConvertSpec<O> for T {
+    // failing impl, if neither are implemented. Tries to link to a (hopefully) nonexistent symbol.
+    default fn try_convert(self) -> Option<O> {
+        unsafe extern "C" {
+            fn __convert_not_implemented() -> !;
+        }
+        unsafe { __convert_not_implemented() }
+    }
+}
+
+/// Sugar around `TryConvert` that just unwraps the Option.
+/// Converts from `Self` to `Output`
 pub trait Convert<Output> {
     fn convert(self) -> Output;
 }
